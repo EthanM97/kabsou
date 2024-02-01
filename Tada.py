1bb0290e556a10ae59ecbf6b4a7a42f6d188c362
Ethan Morissette <55859704+EthanM97@users.noreply.github.com>
2024-02-01 21:59:32 +0000

    Some basics added in

--- Tada.py
+++ Tada.py
@@ -13,10 +13,41 @@
 # limitations under the License.
 
 import streamlit as st
+import pandas as pd
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
+def get_data():
+    user_file = st.file_uploader("Upload your file", type=["csv", "xlsx"]) # User file
+
+    if user_file is not None:
+        # Display a progress bar while the file is being processed
+        with st.spinner('Processing...'):
+            st.success("File uploaded successfully")
+            return user_file
+    else:
+        st.info("Please upload a file")
+        return None
+
+# With user data create visuals
+def visuals(user_file):
+    if user_file is not None:
+        # Read the file into a pandas DataFrame
+        data = pd.read_csv(user_file)
+
+        # Check if the DataFrame is empty
+        if data.empty:
+            st.error("The uploaded file is empty.")
+        else:
+            user_df = pd.DataFrame(data)
+
+            st.write("### Data Preview")
+            st.write(user_df.head())    
+            st.write("---")
+            st.write("### Data Information")
+            st.write(user_df.describe())
+    
 
 def run():
     st.set_page_config(
@@ -27,8 +58,26 @@ def run():
     st.write("# Welcome to Tada! 👋")
 
     st.markdown(
-        """"""  # Empty markdown string to add space between sidebar and content
+        """
+            ---
+            ### What is TADA? 
+        
+            Welcome to TADA, the revolutionary platform that's set to transform your data science journey. 
+            Say goodbye to the tedious and time-consuming initial stages of data work. With TADA, you can
+            effortlessly import your tabular data and simplify your entire workflow, allowing you to focus
+            on insights rather than technical intricacies. But that's not all – TADA goes above and beyond
+            by seamlessly integrating intelligent recommendations into your data exploration process, empowering
+            you to make data-driven decisions with unwavering confidence. Experience the future of data science
+            today with TADA, where efficiency meets intellectual invigoration.
+            
+            ---
+        """
     )
+
+    user_file = get_data() # Get the user data
+    
+    # Display some charts
+    visuals(user_file)
 
 
 if __name__ == "__main__":
